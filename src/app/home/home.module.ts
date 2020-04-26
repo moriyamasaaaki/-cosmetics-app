67d8e14eff61454d81a4bd78cc2eb51bc8de4214
moriyamasaaaki <moriya-7071@outlook.com>
2020-04-27 08:59:52 +0900

    feat(home): モジュール追加 #3

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import { FirstViewComponent } from './first-view/first-view.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, FirstViewComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
