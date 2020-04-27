bc8a7f4b4142295c8f1ec38316beeb18f8f162e3
moriyamasaaaki <moriya-7071@outlook.com>
2020-04-27 10:53:02 +0900

    feat(app,home): shared.module追加 #13

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { FirstViewComponent } from './first-view/first-view.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [HomeComponent, FirstViewComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule,
   ]
 })
 export class HomeModule { }
