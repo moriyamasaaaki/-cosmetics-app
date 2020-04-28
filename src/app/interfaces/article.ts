1979e7a63c75e24c09644f47d2c3bfcc5805ae48
moriyamasaaaki <moriya-7071@outlook.com>
2020-04-28 16:58:02 +0900

    fix(article.ts): firestore.Timestampのimport修正 #41

--- src/app/interfaces/article.ts
+++ src/app/interfaces/article.ts
@@ -1,12 +1,12 @@
-import { Timestamp } from '@google-cloud/firestore';
 import { User } from './user';
+import { firestore } from 'firebase';
 
 export interface Article {
   articleId: string;
   articleImage: string[];
   tag: string;
   content: string;
-  createdAt: Timestamp;
+  createdAt: firestore.Timestamp;
 }
 
 export interface ArticleWithAuthor extends Article {
