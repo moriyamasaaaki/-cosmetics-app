130802851f535ceef00533897acff64a0672e2a0
moriyamasaaaki <moriya-7071@outlook.com>
2020-04-28 10:16:10 +0900

    feat(auth): 認証サービス追加 #16

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth, User } from 'firebase/app';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  afUser$: Observable<User> = this.afAuth.user;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+  ) {
+    this.afUser$.subscribe(user => console.log(user));
+  }
+
+  login() {
+    this.afAuth.signInWithPopup(
+      new auth.GoogleAuthProvider()
+    );
+  }
+}
